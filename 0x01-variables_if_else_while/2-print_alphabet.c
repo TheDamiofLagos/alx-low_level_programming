0c9214f25d8c5bd7b05a56572f9958763cd6c886
thedamioflagos <sunday.damilare@lmu.edu.ng>
2023-08-11 09:48:43 -0700

    correction 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,25 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase
- * followed by a new line.
+ * main - Prints the alphabet.
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-    char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-    int i;
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-    for (i = 0; i < 26; i++)
-    {
-        putchar(alp[i]);
-    }
-    putchar('\n');
-
-    return (0);
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
 }
-
