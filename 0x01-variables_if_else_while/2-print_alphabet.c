d520e78c899a9249c84a4efda1c10350369ba2c6
thedamioflagos <sunday.damilare@lmu.edu.ng>
2023-08-11 09:42:51 -0700

    full commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ * followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+    char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+    int i;
+
+    for (i=0; i<26; i++)
+    {
+        putchar(alp[i]);
+    }
+    putchar('\n');
+
+    return (0);
+}
+
